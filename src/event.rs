2128f59cd019c82bf02c484056e03a8ed2fd95a6
Milad Nekofar <milad@nekofar.com>
2023-09-09 12:14:12 +0330

    refactor: implement an Event struct with getters and setters
    An Event struct is introduced in src/event.rs with key fields like 'kind', 'title' and 'description'. This new struct, along with its getters and setters, enables more effective event-related data handling across the application. It offers easy data manipulation and access, which was lacking in the previous code structure.

---
+++ src/event.rs
@@ -0,0 +1,42 @@
+pub struct Event {
+    kind: String,
+    title: Option<String>,
+    description: Option<String>,
+}
+
+impl Event {
+    // Constructor
+    pub fn new(kind: String, title: Option<String>, description: Option<String>) -> Self {
+        Self {
+            kind,
+            title,
+            description,
+        }
+    }
+
+    // Getters
+    pub fn kind(&self) -> &String {
+        &self.kind
+    }
+
+    pub fn title(&self) -> Option<&String> {
+        self.title.as_ref()
+    }
+
+    pub fn description(&self) -> Option<&String> {
+        self.description.as_ref()
+    }
+
+    // Setters
+    pub fn set_kind(&mut self, kind: String) {
+        self.kind = kind;
+    }
+
+    pub fn set_title(&mut self, title: Option<String>) {
+        self.title = title;
+    }
+
+    pub fn set_description(&mut self, description: Option<String>) {
+        self.description = description;
+    }
+}
