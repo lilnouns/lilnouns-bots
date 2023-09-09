8de13e9c7077c44c882c7e75c3fe83435b25a2be
Milad Nekofar <milad@nekofar.com>
2023-09-09 12:55:25 +0330

    refactor: add `PropHouseDiscordBot` integration to `main` function
    Updated the main function to use the newly implemented PropHouseDiscordBot. This reflects the recent integration with the property management backend, thus enabling a more robust and modular bot management system for the community and auction data.
    
    fixup! refactor: add `PropHouseDiscordBot` integration to `main` function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod event;
+mod discord_bot;
+mod prop_house_discord_bot;
+
+use std::error::Error;
+use discord_bot::DiscordBot;
+use prop_house_discord_bot::PropHouseDiscordBot;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let bot = PropHouseDiscordBot::new();
+    bot.prepare().expect("TODO: panic message");
+
+    Ok(())
 }
