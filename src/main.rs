f5f82a919b4ac751c8eac69c9c37bdd287c45e0d
Milad Nekofar <milad@nekofar.com>
2023-09-10 14:39:11 +0330

    refactor: replace std Error with anyhow Result and include cache module
    Switch to using the `anyhow` library to handle errors in a simpler and more consistent manner, providing helpful context automatically.
    Also, included `cache` module to enhance data handling and persistence. This will enhance the efficiency of requests sent to the bot.
    This will help make the codebase easier to develop on and increase runtime performance.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::error::Error;
+use anyhow::{Context, Result};
 
 use discord_bot::DiscordBot;
 
 use crate::prop_lot_discord_bot::PropLotDiscordBot;
 
+mod cache;
 mod discord_bot;
 mod event;
 mod prop_house_discord_bot;
 mod prop_lot_discord_bot;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     let bot = PropLotDiscordBot::new();
     let source = bot.prepare().await?;
     let events = bot.process(source).await?;
