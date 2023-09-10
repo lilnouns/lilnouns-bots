16da893f617e444554cd4cbbe57d03ae77ca6144
Milad Nekofar <milad@nekofar.com>
2023-09-10 08:49:16 +0330

    refactor: improve main function to improve event handling
    The main function in main.rs was refactored to improve event handling in our bot. The bot now calls prepare(), process(), and dispatch() in sequence, allowing for more granular control of event processing and dispatching. This should enhance bot performance and facilitate debugging.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use std::error::Error;
 use discord_bot::DiscordBot;
 use prop_house_discord_bot::PropHouseDiscordBot;
 
-mod event;
 mod discord_bot;
+mod event;
 mod prop_house_discord_bot;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let bot = PropHouseDiscordBot::new();
-    bot.prepare().await?;
+    let source = bot.prepare().await?;
+    let events = bot.process(source).await?;
+    bot.dispatch(events).await?;
 
     Ok(())
 }
