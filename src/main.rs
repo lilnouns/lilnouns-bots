ddcadcf214fc1579ba73048f98ae8ba37e00afa0
Milad Nekofar <milad@nekofar.com>
2023-09-12 23:26:55 +0330

    refactor: improve and modularize Prop Lot fetching logic
    The logic for fetching PropLot ideas has been moved from the main.rs file to a new fetcher.rs file within a new prop_lot directory. This is done to encapsulate the related functionalities and improve the readability of the code. Further, the GraphQL related entities such as 'Query' and 'Idea' have been renamed. This would improve understanding of the code and enhance maintainability.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
-use std::env;
+use anyhow::Result;
 
-use anyhow::{anyhow, Context, Result};
-use graphql_client::{reqwest::post_graphql, GraphQLQuery};
-use reqwest::Client;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "graphql/schemas/prop_lot_schema.graphql",
-    query_path = "graphql/queries/prop_lot_query.graphql",
-    request_derives = "Debug",
-    response_derives = "Debug",
-    variables_derives = "Debug",
-    deprecated = "warn"
-)]
-struct PropLot;
-
-type Date = String;
-type Idea = prop_lot::PropLotIdeas;
+mod prop_lot;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let ideas = fetch_prop_lot_ideas().await?;
+    let ideas = prop_lot::fetch_ideas().await?;
 
     let ideas_ids: Vec<String> = ideas.iter().map(|i| i.id.to_string()).collect();
     let joined_ids = ideas_ids.join(",");
@@ -28,33 +12,4 @@ async fn main() -> Result<()> {
     println!("All ideas ids({})", joined_ids);
 
     Ok(())
-}
-
-async fn fetch_prop_lot_ideas() -> Result<Vec<Idea>> {
-    let url = match env::var("PROP_LOT_GRAPHQL_URL") {
-        Ok(val) => val,
-        Err(_) => return Err(anyhow!("PROP_LOT_GRAPHQL_URL is not set in env")),
-    };
-
-    let variables = prop_lot::Variables {
-        options: prop_lot::IdeaInputOptions {
-            idea_id: None,
-            sort: Some(prop_lot::SORT_TYPE::LATEST),
-        },
-    };
-
-    let client = Client::new();
-
-    let response = post_graphql::<PropLot, _>(&client, url.to_string(), variables)
-        .await
-        .context("Failed to execute GraphQL request")?;
-
-    let ideas = match response.data {
-        Some(data) => data
-            .ideas
-            .ok_or(anyhow!("Ideas not found in the response data"))?,
-        None => return Err(anyhow!("Response data is unavailable")),
-    };
-
-    Ok(ideas)
 }
