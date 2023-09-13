dff87bbc8710314dd9840d2587abbf4318092b36
Milad Nekofar <milad@nekofar.com>
2023-09-13 21:47:46 +0330

    refactor: improve `prop_lot` setup function to simplify async call
    This commit makes adjustments to the setup function for better, cleaner handling of async calls while fetching ideas. The diff removes the usage of match block and the unnecessary error log import by directly calling the fetch_ideas() function and handling its output.
    
    The output is now directly used to build up tasks to be handled asynchronously. This eliminates excessive error handling and makes the code more readable. The error log import was unnecessary because we don't bail out but continue the execution even if no ideas are found.

--- src/prop_lot/mod.rs
+++ src/prop_lot/mod.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use futures::future::join_all;
-use log::{error, info};
+use log::info;
 
 pub use fetcher::fetch_ideas;
 
@@ -12,26 +12,25 @@ mod fetcher;
 mod handler;
 
 pub async fn setup() {
-    match fetch_ideas().await {
-        Some(ideas) => {
-            let ideas_ids: Vec<String> = ideas.iter().map(|i| i.id.to_string()).collect();
-            info!("Fetched ideas ids({})", ideas_ids.join(","));
+    let ideas = fetch_ideas().await;
 
-            let mut tasks = Vec::new();
+    if let Some(idea_list) = ideas {
+        let ideas_ids: Vec<String> = idea_list.iter().map(|i| i.id.to_string()).collect();
+        info!("Fetched ideas ids({})", ideas_ids.join(","));
 
-            for idea in ideas {
-                let arc_idea = Arc::new(idea);
-                let task = tokio::spawn({
-                    let arc_idea = Arc::clone(&arc_idea);
-                    async move {
-                        set_idea_cache(&*arc_idea).await.unwrap();
-                    }
-                });
-                tasks.push(task);
-            }
+        let mut tasks = Vec::new();
 
-            join_all(tasks).await;
+        for idea in idea_list {
+            let arc_idea = Arc::new(idea);
+            let task = tokio::spawn({
+                let arc_idea = Arc::clone(&arc_idea);
+                async move {
+                    set_idea_cache(&*arc_idea).await.unwrap();
+                }
+            });
+            tasks.push(task);
         }
-        None => error!("Error: No ideas found"), // don't bail, just print an error
-    };
+
+        join_all(tasks).await;
+    }
 }
