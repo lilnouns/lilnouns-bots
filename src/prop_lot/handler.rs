2dd57bdac5f9a0f9c3a67593dbd2471c6240bf48
Milad Nekofar <milad@nekofar.com>
2023-09-14 21:03:02 +0330

    refactor: add functionality to handle new ideas in Prop Lot
    This commit introduces substantial changes to the 'handle_new_idea' function
    in the 'handler' module of 'prop_lot'. The function now fetches the base url
    and the discord webhook url from the environment variables, creates a new webhook
    using these, and executes it. This enables the dispatch of new idea notifications
    to the discord channel directly, resulting in quicker updates. The notification includes
    the title, description and unique url to access each new idea. I have
    also introduced error handling for both failure scenarios: missing environment variables
    and network issues. It adds asynchronous context that returns specific error
    messages. The diff also adds additional required imports for this functionality.
    
    fixup! refactor: add functionality to handle new ideas in Prop Lot

--- src/prop_lot/handler.rs
+++ src/prop_lot/handler.rs
@@ -1,5 +1,35 @@
+use std::env;
+
+use anyhow::Context;
+use anyhow::Result;
+use serenity::http::Http;
+use serenity::model::channel::Embed;
+use serenity::model::webhook::Webhook;
+
 use crate::prop_lot::fetcher::Idea;
 
-pub async fn handle_new_idea(idea: &Idea) {
-    // set_idea_cache(idea).await.unwrap();
+pub async fn handle_new_idea(idea: &Idea) -> Result<()> {
+    let base_url = env::var("PROP_LOT_BASE_URL").context("PROP_LOT_BASE_URL is not set in env")?;
+
+    let webhook_url = env::var("PROP_LOT_DISCORD_WEBHOOK_URL")
+        .context("PROP_LOT_DISCORD_WEBHOOK_URL is not set in env")?;
+
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, webhook_url.as_str())
+        .await
+        .context("Failed to create webhook from URL")?;
+
+    let message = Embed::fake(|e| {
+        e.title(format!("New Idea: {}", idea.title))
+            .url(format!("{}/idea/{}", base_url, idea.id))
+            .description(&idea.tldr)
+            .colour(0xFFB911)
+    });
+
+    webhook
+        .execute(&http, false, |w| w.embeds(vec![message]))
+        .await
+        .context("Failed to execute webhook")?;
+
+    Ok(())
 }
