240d5053b46bfe8a369a30e190ef678075b879a2
Milad Nekofar <milad@nekofar.com>
2023-09-16 23:04:08 +0330

    refactor: add caching for votes and comments of Prop Lot
    The caching feature has been extended to now include votes and comments. Additional import statements for fetching and setting cache for votes and comments have been added. For each vote or comment fetched, a new task is spawned that attempts to cache the fetched data. Errors during the caching process are now being caught and logged. This addition improves the performance and responsiveness of the application by reducing unnecessary data fetching.

--- src/prop_lot/mod.rs
+++ src/prop_lot/mod.rs
@@ -5,7 +5,8 @@ use log::{error, info};
 
 pub use fetcher::fetch_ideas;
 
-use crate::prop_lot::cacher::{get_idea_cache, set_idea_cache};
+use crate::prop_lot::cacher::{get_idea_cache, set_comment_cache, set_idea_cache, set_vote_cache};
+use crate::prop_lot::fetcher::{fetch_comments, fetch_votes};
 use crate::prop_lot::handler::handle_new_idea;
 
 mod cacher;
@@ -24,6 +25,48 @@ pub async fn setup() {
                     info!("Cache a new idea... ({:?})", arc_idea.id);
                     let _ = set_idea_cache(&*arc_idea).await.map_err(|e| {
                         error!("Error while trying to set idea cache: {}", e);
+                    });
+                }
+            });
+
+            tasks.push(task);
+        }
+
+        join_all(tasks).await;
+    }
+
+    if let Some(votes) = fetch_votes().await {
+        let mut tasks = Vec::new();
+
+        for vote in votes {
+            let arc_vote = Arc::new(vote);
+            let task = tokio::spawn({
+                let arc_vote = Arc::clone(&arc_vote);
+                async move {
+                    info!("Cache a new vote... ({:?})", arc_vote.id);
+                    let _ = set_vote_cache(&*arc_vote).await.map_err(|e| {
+                        error!("Error while trying to set vote cache: {}", e);
+                    });
+                }
+            });
+
+            tasks.push(task);
+        }
+
+        join_all(tasks).await;
+    }
+
+    if let Some(comments) = fetch_comments().await {
+        let mut tasks = Vec::new();
+
+        for comment in comments {
+            let arc_comment = Arc::new(comment);
+            let task = tokio::spawn({
+                let arc_comment = Arc::clone(&arc_comment);
+                async move {
+                    info!("Cache a new comment... ({:?})", arc_comment.id);
+                    let _ = set_comment_cache(&*arc_comment).await.map_err(|e| {
+                        error!("Error while trying to set comment cache: {}", e);
                     });
                 }
             });
