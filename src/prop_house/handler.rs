593a705d7832c484d40f45a0bb8d4bf57a29f9d4
Milad Nekofar <milad@nekofar.com>
2023-09-18 13:49:28 +0330

    refactor: update notification handling for new Prop House proposals and votes
    Modified the `handler.rs` file to improve the handling of new proposals and votes. Previously, log messages were written for new proposals and votes, but they now trigger notifications via Discord webhooks. The notifications include relevant details such as the proposal title/vote direction, proposal summary, and a link to the proposal. This provides more immediate and interact-able updates about new proposals and votes.
    
    In addition, environment variable handling for the base URL and webhook URL was moved to this file.

--- src/prop_house/handler.rs
+++ src/prop_house/handler.rs
@@ -1,13 +1,12 @@
 use std::env;
 
 use anyhow::{Context, Result};
-use log::{debug, error};
 use serenity::http::Http;
 use serenity::model::channel::Embed;
 use serenity::model::webhook::Webhook;
 
 use crate::prop_house::cacher::{
-    get_proposal_cache, set_auction_cache, set_proposal_cache, set_vote_cache,
+    get_auction_cache, get_proposal_cache, set_auction_cache, set_proposal_cache, set_vote_cache,
 };
 use crate::prop_house::fetcher::{Auction, Proposal, Vote};
 
@@ -45,17 +44,96 @@ pub(crate) async fn handle_new_auction(auction: &Auction) -> Result<()> {
 }
 
 pub(crate) async fn handle_new_proposal(proposal: &Proposal) -> Result<()> {
-    debug!("New Proposal: {}", proposal.title);
-    set_proposal_cache(proposal).unwrap();
+    let auction = get_auction_cache(proposal.auction_id)?
+        .ok_or_else(|| anyhow::anyhow!("No auction found for id {}", proposal.auction_id))?;
+
+    let base_url =
+        env::var("PROP_HOUSE_BASE_URL").context("PROP_HOUSE_BASE_URL is not set in env")?;
+
+    let url = env::var("PROP_HOUSE_DISCORD_WEBHOOK_URL")
+        .context("PROP_HOUSE_DISCORD_WEBHOOK_URL is not set in env")?;
+
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, url.as_str())
+        .await
+        .context("Failed to create webhook from URL")?;
+
+    let message = Embed::fake(|e| {
+        e.author(|a| {
+            a.name(format!(
+                "{}...{}",
+                &proposal.address[0..4],
+                &proposal.address[38..42]
+            ))
+        })
+        .title(format!("New Proposal: {}", proposal.title))
+        .url(format!(
+            "{}/{}/{}",
+            base_url,
+            auction.title.replace(' ', "-").to_lowercase(),
+            proposal.id
+        ))
+        .description(&proposal.tldr)
+        .colour(0x8A2CE2)
+    });
+
+    webhook
+        .execute(&http, false, |w| w.embeds(vec![message]))
+        .await
+        .context("Failed to execute webhook")?;
+
+    set_proposal_cache(proposal)?;
+
     Ok(())
 }
 
 pub(crate) async fn handle_new_vote(vote: &Vote) -> Result<()> {
-    if let Ok(Some(proposal)) = get_proposal_cache(vote.proposal_id) {
-        debug!("New Vote on Proposal: {}", proposal.title)
-    } else {
-        error!("No proposal found for given id: {}", vote.proposal_id);
-    }
+    let proposal = get_proposal_cache(vote.proposal_id)?
+        .ok_or_else(|| anyhow::anyhow!("No proposal found for id {}", vote.proposal_id))?;
+
+    let base_url =
+        env::var("PROP_HOUSE_BASE_URL").context("PROP_HOUSE_BASE_URL is not set in env")?;
+
+    let url = env::var("PROP_HOUSE_DISCORD_WEBHOOK_URL")
+        .context("PROP_HOUSE_DISCORD_WEBHOOK_URL is not set in env")?;
+
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, url.as_str())
+        .await
+        .context("Failed to create webhook from URL")?;
+
+    let message = Embed::fake(|e| {
+        e.author(|a| {
+            a.name(format!(
+                "{}...{}",
+                &vote.address[0..4],
+                &vote.address[38..42]
+            ))
+        })
+        .title(format!(
+            "New Vote {}: {}",
+            match vote.direction {
+                1 => "For",
+                _ => "Against",
+            },
+            proposal.title
+        ))
+        .url(format!(
+            "{}/{}/{}",
+            base_url,
+            proposal.title.replace(' ', "-").to_lowercase(),
+            proposal.id
+        ))
+        .description(&proposal.tldr)
+        .colour(0x8A2CE2)
+    });
+
+    webhook
+        .execute(&http, false, |w| w.embeds(vec![message]))
+        .await
+        .context("Failed to execute webhook")?;
+
     set_vote_cache(vote).unwrap();
+
     Ok(())
 }
