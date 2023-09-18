6196ba43865b688e7bab7ab46f03bce64e68d132
Milad Nekofar <milad@nekofar.com>
2023-09-18 15:46:26 +0330

    refactor: add author id and body to Prop Lot comment struct
    The Comment struct in fetcher.rs has been extended with author_id and body. With this change, each comment fetched will now have associated author and content details, enhancing the granularity and usability of the data.

--- src/prop_lot/fetcher.rs
+++ src/prop_lot/fetcher.rs
@@ -58,6 +58,8 @@ pub(crate) struct Vote {
 pub(crate) struct Comment {
     pub(crate) id: isize,
     pub(crate) idea_id: isize,
+    pub(crate) author_id: String,
+    pub(crate) body: String,
 }
 
 async fn fetch<QueryType: GraphQLQuery>(
@@ -158,6 +160,8 @@ pub(crate) async fn fetch_comments() -> Option<Vec<Comment>> {
         .map(|comment| Comment {
             id: comment.id.try_into().unwrap(),
             idea_id: comment.id.try_into().unwrap(),
+            author_id: comment.author_id.clone(),
+            body: comment.body.clone(),
         })
         .collect();
 
