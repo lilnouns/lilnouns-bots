2cd15c5854dd2c30eaaa23019c6c3bfa7193e382
Milad Nekofar <milad@nekofar.com>
2023-09-21 20:10:08 +0330

    refactor: improve caching and fetching logic in prop house module
    This change simplifies the caching strategy in the prop_house module. Instead of storing each auction, proposal, and vote as an individual key-value pair, this commit groups them under collective keys. This approach reduces the clutter in the cache and makes it easier to retrieve and manage related entities. Additionally, a new logic is added to identify and handle new entries, improving the efficiency and clarity of the code.

--- src/prop_house/mod.rs
+++ src/prop_house/mod.rs
@@ -11,33 +11,15 @@ pub mod handler;
 
 pub async fn setup(cache: &Cache, fetcher: &GraphQLFetcher) -> Result<()> {
     if let Some(auctions) = fetcher.fetch_auctions().await {
-        for auction in auctions {
-            cache
-                .put(
-                    &format!("{}{}", "PROP_HOUSE_AUCTION_", auction.id),
-                    &auction,
-                )
-                .await;
-        }
+        cache.put("prop_house:auctions", &auctions).await;
     }
 
     if let Some(proposals) = fetcher.fetch_proposals().await {
-        for proposal in proposals {
-            cache
-                .put(
-                    &format!("{}{}", "PROP_HOUSE_PROPOSAL_", proposal.id),
-                    &proposal,
-                )
-                .await;
-        }
+        cache.put("prop_house:proposals", &proposals).await;
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
-        for vote in votes {
-            cache
-                .put(&format!("{}{}", "PROP_HOUSE_VOTE_", vote.id), &vote)
-                .await;
-        }
+        cache.put("prop_house:votes", &votes).await;
     }
 
     Ok(())
@@ -49,48 +31,57 @@ pub async fn start(
     handler: &DiscordHandler,
 ) -> Result<()> {
     if let Some(auctions) = fetcher.fetch_auctions().await {
-        for auction in auctions {
-            let cached_auction: Option<Auction> = cache
-                .get(&format!("{}{}", "PROP_HOUSE_AUCTION_", auction.id))
-                .await?;
+        if let Some(old_auctions) = cache.get::<Vec<Auction>>("prop_house:auctions").await? {
+            let old_ids: Vec<_> = old_auctions.iter().map(|auction| &auction.id).collect();
+            let new_auctions: Vec<_> = auctions
+                .iter()
+                .filter(|auction| !old_ids.contains(&&auction.id))
+                .collect();
 
-            if cached_auction.is_none() {
+            for auction in new_auctions {
                 info!("Handle a new auction... ({:?})", auction.id);
-                if let Err(err) = handler.handle_new_auction(&auction).await {
+                if let Err(err) = handler.handle_new_auction(auction).await {
                     error!("Failed to handle new auction: {:?}", err);
                 }
             }
         }
+        cache.put("prop_house:auctions", &auctions).await;
     }
 
     if let Some(proposals) = fetcher.fetch_proposals().await {
-        for proposal in proposals {
-            let cached_proposal: Option<Proposal> = cache
-                .get(&format!("{}{}", "PROP_HOUSE_PROPOSAL_", proposal.id))
-                .await?;
+        if let Some(old_proposals) = cache.get::<Vec<Proposal>>("prop_house:proposals").await? {
+            let old_ids: Vec<_> = old_proposals.iter().map(|proposal| &proposal.id).collect();
+            let new_proposals: Vec<_> = proposals
+                .iter()
+                .filter(|proposal| !old_ids.contains(&&proposal.id))
+                .collect();
 
-            if cached_proposal.is_none() {
+            for proposal in new_proposals {
                 info!("Handle a new proposal... ({:?})", proposal.id);
-                if let Err(err) = handler.handle_new_proposal(&proposal).await {
+                if let Err(err) = handler.handle_new_proposal(proposal).await {
                     error!("Failed to handle new proposal: {:?}", err);
                 }
             }
         }
+        cache.put("prop_lot:proposals", &proposals).await;
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
-        for vote in votes {
-            let cached_vote: Option<Vote> = cache
-                .get(&format!("{}{}", "PROP_HOUSE_VOTE_", vote.id))
-                .await?;
+        if let Some(old_votes) = cache.get::<Vec<Vote>>("prop_lot:votes").await? {
+            let old_ids: Vec<_> = old_votes.iter().map(|vote| &vote.id).collect();
+            let new_votes: Vec<_> = votes
+                .iter()
+                .filter(|vote| !old_ids.contains(&&vote.id))
+                .collect();
 
-            if cached_vote.is_none() {
+            for vote in new_votes {
                 info!("Handle a new vote... ({:?})", vote.id);
-                if let Err(err) = handler.handle_new_vote(&vote).await {
+                if let Err(err) = handler.handle_new_vote(vote).await {
                     error!("Failed to handle new vote: {:?}", err);
                 }
             }
         }
+        cache.put("prop_house:votes", &votes).await;
     }
 
     Ok(())
