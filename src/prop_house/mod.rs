33ca6ba35233e5d9160c5da8432b29ee233da559
Milad Nekofar <milad@nekofar.com>
2023-09-21 20:35:04 +0330

    refactor: improve logging for fetch and handler operations
    Logging has been enhanced in `prop_house` and `prop_lot` modules. Logging statements for the debug and warn level were added to better trace the state and outcome of fetch operations and handling of new items. Each operation of fetching and handling of auctions, proposals, votes, ideas, and comments now has corresponding logging. Now, when fetch operations start, complete, or fail, it is logged, which will assist us in performance monitoring and error tracing. Similarly, the handling of new items including success and failure scenarios are better traceable.

--- src/prop_house/mod.rs
+++ src/prop_house/mod.rs
@@ -1,4 +1,4 @@
-use log::{error, info};
+use log::{debug, error, info, warn};
 use worker::Result;
 
 use fetcher::{Auction, GraphQLFetcher, Proposal, Vote};
@@ -10,18 +10,33 @@ pub mod fetcher;
 pub mod handler;
 
 pub async fn setup(cache: &Cache, fetcher: &GraphQLFetcher) -> Result<()> {
+    debug!("Setup function started.");
+
     if let Some(auctions) = fetcher.fetch_auctions().await {
+        info!("Fetched {:?} auctions.", auctions.len());
+        debug!("Putting fetched auctions into cache.");
         cache.put("prop_house:auctions", &auctions).await;
+    } else {
+        warn!("Failed to fetch auctions");
     }
 
     if let Some(proposals) = fetcher.fetch_proposals().await {
+        info!("Fetched {:?} proposals.", proposals.len());
+        debug!("Putting fetched proposals into cache.");
         cache.put("prop_house:proposals", &proposals).await;
+    } else {
+        warn!("Failed to fetch proposals");
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
+        info!("Fetched {:?} votes.", votes.len());
+        debug!("Putting fetched votes into cache.");
         cache.put("prop_house:votes", &votes).await;
+    } else {
+        warn!("Failed to fetch votes");
     }
 
+    debug!("Setup function finished.");
     Ok(())
 }
 
@@ -30,7 +45,11 @@ pub async fn start(
     fetcher: &GraphQLFetcher,
     handler: &DiscordHandler,
 ) -> Result<()> {
+    debug!("Start function started.");
+
     if let Some(auctions) = fetcher.fetch_auctions().await {
+        debug!("Fetched {:?} auctions.", auctions.len());
+
         if let Some(old_auctions) = cache.get::<Vec<Auction>>("prop_house:auctions").await? {
             let old_ids: Vec<_> = old_auctions.iter().map(|auction| &auction.id).collect();
             let new_auctions: Vec<_> = auctions
@@ -38,17 +57,26 @@ pub async fn start(
                 .filter(|auction| !old_ids.contains(&&auction.id))
                 .collect();
 
+            debug!("Found {:?} new auctions.", new_auctions.len());
+
             for auction in new_auctions {
-                info!("Handle a new auction... ({:?})", auction.id);
+                info!("Handling a new auction...");
                 if let Err(err) = handler.handle_new_auction(auction).await {
                     error!("Failed to handle new auction: {:?}", err);
+                } else {
+                    debug!("Successfully handled new auction: {:?}", auction.id);
                 }
             }
         }
         cache.put("prop_house:auctions", &auctions).await;
+        info!("Updated auctions in cache");
+    } else {
+        warn!("Failed to fetch auctions");
     }
 
     if let Some(proposals) = fetcher.fetch_proposals().await {
+        debug!("Fetched {:?} proposals.", proposals.len());
+
         if let Some(old_proposals) = cache.get::<Vec<Proposal>>("prop_house:proposals").await? {
             let old_ids: Vec<_> = old_proposals.iter().map(|proposal| &proposal.id).collect();
             let new_proposals: Vec<_> = proposals
@@ -56,33 +84,51 @@ pub async fn start(
                 .filter(|proposal| !old_ids.contains(&&proposal.id))
                 .collect();
 
+            debug!("Found {:?} new proposals.", new_proposals.len());
+
             for proposal in new_proposals {
-                info!("Handle a new proposal... ({:?})", proposal.id);
+                info!("Handling a new proposal... ({:?})", proposal.id);
                 if let Err(err) = handler.handle_new_proposal(proposal).await {
                     error!("Failed to handle new proposal: {:?}", err);
+                } else {
+                    debug!("Successfully handled new proposal: {:?}", proposal.id);
                 }
             }
         }
-        cache.put("prop_lot:proposals", &proposals).await;
+        cache.put("prop_house:proposals", &proposals).await;
+        info!("Updated proposals in cache");
+    } else {
+        warn!("Failed to fetch proposals");
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
-        if let Some(old_votes) = cache.get::<Vec<Vote>>("prop_lot:votes").await? {
+        debug!("Fetched {:?} votes.", votes.len());
+
+        if let Some(old_votes) = cache.get::<Vec<Vote>>("prop_house:votes").await? {
             let old_ids: Vec<_> = old_votes.iter().map(|vote| &vote.id).collect();
             let new_votes: Vec<_> = votes
                 .iter()
                 .filter(|vote| !old_ids.contains(&&vote.id))
                 .collect();
 
+            debug!("Found {:?} new votes.", new_votes.len());
+
             for vote in new_votes {
-                info!("Handle a new vote... ({:?})", vote.id);
+                info!("Handling a new vote...");
                 if let Err(err) = handler.handle_new_vote(vote).await {
                     error!("Failed to handle new vote: {:?}", err);
+                } else {
+                    debug!("Successfully handled new vote: {:?}", vote.id);
                 }
             }
         }
         cache.put("prop_house:votes", &votes).await;
+        info!("Updated votes in cache");
+    } else {
+        warn!("Failed to fetch votes");
     }
+
+    debug!("Start function finished.");
 
     Ok(())
 }
--- src/prop_lot/mod.rs
+++ src/prop_lot/mod.rs
@@ -1,7 +1,7 @@
-use log::{error, info};
+use log::{debug, error, info, warn};
 use worker::Result;
 
-use fetcher::{Comment, GraphQLFetcher, Idea, Vote};
+use fetcher::{GraphQLFetcher, Idea, Vote};
 use handler::DiscordHandler;
 
 use crate::cache::Cache;
@@ -10,18 +10,32 @@ pub mod fetcher;
 pub mod handler;
 
 pub async fn setup(cache: &Cache, fetcher: &GraphQLFetcher) -> Result<()> {
+    debug!("Setup function started.");
     if let Some(ideas) = fetcher.fetch_ideas().await {
+        info!("Fetched {:?} idea.", ideas.len());
+        debug!("Putting fetched ideas into cache.");
         cache.put("prop_lot:ideas", &ideas).await;
+    } else {
+        warn!("Failed to fetch ideas");
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
+        info!("Fetched {:?} votes.", votes.len());
+        debug!("Putting fetched votes into cache.");
         cache.put("prop_lot:votes", &votes).await;
+    } else {
+        warn!("Failed to fetch votes");
     }
 
     if let Some(comments) = fetcher.fetch_comments().await {
+        info!("Fetched {:?} comments.", comments.len());
+        debug!("Putting fetched comments into cache.");
         cache.put("prop_lot:comments", &comments).await;
+    } else {
+        warn!("Failed to fetch comments");
     }
 
+    debug!("Setup function finished.");
     Ok(())
 }
 
@@ -30,7 +44,11 @@ pub async fn start(
     fetcher: &GraphQLFetcher,
     handler: &DiscordHandler,
 ) -> Result<()> {
+    debug!("Start function started.");
+
     if let Some(ideas) = fetcher.fetch_ideas().await {
+        debug!("Fetched {:?} ideas.", ideas.len());
+
         if let Some(old_ideas) = cache.get::<Vec<Idea>>("prop_lot:ideas").await? {
             let old_ids: Vec<_> = old_ideas.iter().map(|idea| &idea.id).collect();
             let new_ideas: Vec<_> = ideas
@@ -38,17 +56,26 @@ pub async fn start(
                 .filter(|idea| !old_ids.contains(&&idea.id))
                 .collect();
 
+            debug!("Found {:?} new ideas.", new_ideas.len());
+
             for idea in new_ideas {
-                info!("Handle a new idea... ({:?})", idea.id);
+                info!("Handle a new idea...");
                 if let Err(err) = handler.handle_new_idea(idea).await {
                     error!("Failed to handle new idea: {:?}", err);
+                } else {
+                    debug!("Successfully handled new idea: {:?}", idea.id);
                 }
             }
         }
         cache.put("prop_lot:ideas", &ideas).await;
+        info!("Updated ideas in cache");
+    } else {
+        warn!("Failed to fetch ideas");
     }
 
     if let Some(votes) = fetcher.fetch_votes().await {
+        debug!("Fetched {:?} votes.", votes.len());
+
         if let Some(old_votes) = cache.get::<Vec<Vote>>("prop_lot:votes").await? {
             let old_ids: Vec<_> = old_votes.iter().map(|vote| &vote.id).collect();
             let new_votes: Vec<_> = votes
@@ -56,33 +83,51 @@ pub async fn start(
                 .filter(|vote| !old_ids.contains(&&vote.id))
                 .collect();
 
+            debug!("Found {:?} new votes.", new_votes.len());
+
             for vote in new_votes {
-                info!("Handle a new vote... ({:?})", vote.id);
+                info!("Handling a new vote...");
                 if let Err(err) = handler.handle_new_vote(vote).await {
                     error!("Failed to handle new vote: {:?}", err);
+                } else {
+                    debug!("Successfully handled new vote: {:?}", vote.id);
                 }
             }
         }
         cache.put("prop_lot:votes", &votes).await;
+        info!("Updated votes in cache");
+    } else {
+        warn!("Failed to fetch votes");
     }
 
     if let Some(comments) = fetcher.fetch_comments().await {
-        if let Some(old_comments) = cache.get::<Vec<Comment>>("prop_lot:comments").await? {
+        debug!("Fetched {:?} comments.", comments.len());
+
+        if let Some(old_comments) = cache.get::<Vec<Vote>>("prop_lot:comments").await? {
             let old_ids: Vec<_> = old_comments.iter().map(|comment| &comment.id).collect();
             let new_comments: Vec<_> = comments
                 .iter()
                 .filter(|comment| !old_ids.contains(&&comment.id))
                 .collect();
 
+            debug!("Found {:?} new comments.", new_comments.len());
+
             for comment in new_comments {
-                info!("Handle a new comment... ({:?})", comment.id);
+                info!("Handling a new comment...");
                 if let Err(err) = handler.handle_new_comment(comment).await {
                     error!("Failed to handle new comment: {:?}", err);
+                } else {
+                    debug!("Successfully handled new comment: {:?}", comment.id);
                 }
             }
         }
         cache.put("prop_lot:comments", &comments).await;
+        info!("Updated comments in cache");
+    } else {
+        warn!("Failed to fetch comments");
     }
+
+    debug!("Start function finished.");
 
     Ok(())
 }
