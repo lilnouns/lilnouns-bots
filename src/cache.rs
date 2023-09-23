56653211eb0b78fd61ade3840a7441856e78fbcd
Milad Nekofar <milad@nekofar.com>
2023-09-23 12:47:56 +0330

    refactor: remove unnecessary logging in handlers and cache
    Few info and debug log lines were removed across handler.rs files in prop_lot and prop_house modules, and in cache.rs. These lines were mostly logging procedural info such as initialization of classes, running functions etc. which are not necessary and clutter the log files. Unnecessary logging might distract from truly important errors or information. Therefore, only essential error log lines have been retained.

--- src/cache.rs
+++ src/cache.rs
@@ -1,4 +1,4 @@
-use log::{debug, error};
+use log::error;
 use serde::de::DeserializeOwned;
 use serde::ser::Serialize;
 use worker::kv::{KvError, KvStore};
@@ -10,12 +10,10 @@ pub struct Cache {
 
 impl Cache {
     pub fn new(store: KvStore) -> Self {
-        debug!("Creating new Cache instance");
         Self { store }
     }
 
     pub fn from(env: &Env) -> Self {
-        debug!("Creating Cache from Env");
         let store_name = env.var("KV_STORE_NAME").unwrap().to_string();
         let store = env.kv(&store_name).unwrap();
 
@@ -23,23 +21,18 @@ impl Cache {
     }
 
     pub async fn put<T: Serialize>(&self, key: &str, value: &T) {
-        debug!("Putting value in KV Store");
         if let Ok(put) = self.store.put(key, value) {
             if let Err(pe) = put.execute().await {
                 error!("Failed updating KV: {}", pe);
-            } else {
-                debug!("Successfully updated KV");
             }
         }
     }
 
     pub async fn get<T: DeserializeOwned>(&self, key: &str) -> Result<Option<T>, KvError> {
-        debug!("Getting value from KV Store");
         self.store.get(key).json::<T>().await
     }
 
     pub async fn has(&self, key: &str) -> bool {
-        debug!("Checking if key exists in KV Store");
         match self.get::<String>(key).await {
             Ok(val) => val.is_some(),
             Err(_) => false,
