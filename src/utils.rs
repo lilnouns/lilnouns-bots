cec4ebd247c396d3ce5521553d205bc53ebefc24
Milad Nekofar <milad@nekofar.com>
2023-09-24 17:43:28 +0330

    refactor: rename function and add address helper functions
    Renamed the function `get_ens` to `get_domain_name` for better clarity and added two new utilities `get_short_address` and `get_explorer_address` for address processing. These changes will help to enhance readability and improve address management in the application.

--- src/utils.rs
+++ src/utils.rs
@@ -7,7 +7,7 @@ use ethers::{
     types::Address,
 };
 
-pub async fn get_ens(address: &str) -> Result<String> {
+pub async fn get_domain_name(address: &str) -> Result<String> {
     let endpoint = "https://eth.llamarpc.com";
     let provider = Provider::<Http>::try_from(endpoint)
         .map_err(|error| anyhow!("Failed to create provider from endpoint: {}", error))?;
@@ -21,4 +21,26 @@ pub async fn get_ens(address: &str) -> Result<String> {
         .map_err(|error| anyhow!("Failed to lookup address: {}", error))?;
 
     Ok(domain_name)
+}
+
+pub fn get_short_address(address: &str) -> String {
+    let first_four: String = address.chars().take(4).collect();
+    let last_four: String = address
+        .chars()
+        .rev()
+        .take(4)
+        .collect::<Vec<char>>()
+        .iter()
+        .rev()
+        .collect();
+
+    if first_four.len() == 4 && last_four.len() == 4 {
+        format!("{}...{}", first_four, last_four)
+    } else {
+        address.to_string()
+    }
+}
+
+pub fn get_explorer_address(address: &str) -> String {
+    format!("https://etherscan.io/address/{}", address)
 }
