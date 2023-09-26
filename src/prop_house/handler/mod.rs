a453df5435429b3c943afab74a05c21de0fa7b5b
Milad Nekofar <milad@nekofar.com>
2023-09-26 17:33:23 +0330

    refactor: update handlers to support multiple types
    The code has been refactored to support multiple types of handlers. Previously, it was restricted to only a DiscordHandler. However, a need arose to add another handler and hence the code was adjusted to allow any number of handlers by storing them in a vector. This change allows for flexibility in adding or removing handlers without requiring code changes. Now, the handling of new auctions, votes, and proposals is done by all handlers in the vector. The commit also includes the addition of FarcasterHandler to the handlers vector.

--- src/prop_house/handler/mod.rs
+++ src/prop_house/handler/mod.rs
@@ -3,10 +3,10 @@ use async_trait::async_trait;
 use crate::prop_house::fetcher::{Auction, Proposal, Vote};
 
 pub(crate) mod discord;
-mod farcaster;
+pub(crate) mod farcaster;
 
 #[async_trait(? Send)]
-pub(crate) trait Handler {
+pub trait Handler {
     async fn handle_new_auction(&self, auction: &Auction) -> worker::Result<()>;
     async fn handle_new_proposal(&self, proposal: &Proposal) -> worker::Result<()>;
     async fn handle_new_vote(&self, vote: &Vote) -> worker::Result<()>;
