db519723969a123eed4696ac4f7f7742aa754ed3
Milad Nekofar <milad@nekofar.com>
2023-09-26 14:34:22 +0330

    refactor: add Handler trait to `prop_lot` module
    This update introduces the 'Handler' trait to the 'prop_lot' module. It includes the implementation of three async methods - 'handle_new_idea', 'handle_new_vote', and 'handle_new_comment'. These methods aim to provide the functionality of handling new entries in idea, vote, and comment respectively. This new feature was added to improve event handling in the application.

--- src/prop_lot/handler/mod.rs
+++ src/prop_lot/handler/mod.rs
@@ -1,2 +1,13 @@
+use async_trait::async_trait;
+
+use crate::prop_lot::fetcher::{Comment, Idea, Vote};
+
 pub(crate) mod discord;
 pub(crate) mod farcaster;
+
+#[async_trait]
+pub(crate) trait Handler {
+    async fn handle_new_idea(&self, idea: &Idea) -> worker::Result<()>;
+    async fn handle_new_vote(&self, vote: &Vote) -> worker::Result<()>;
+    async fn handle_new_comment(&self, comment: &Comment) -> worker::Result<()>;
+}
