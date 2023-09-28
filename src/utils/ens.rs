6ddbce4353bcd09ee425fd245afad6f74807f945
Milad Nekofar <milad@nekofar.com>
2023-09-28 21:54:31 +0330

    refactor: update provider creation logic for ENS utility
    Updated the provider creation process within utils/ens.rs to enhance code readability and maintainability. Introduced a constant for the Ethereum RPC URL and created a standalone async function, 'create_provider', to initiate the ethers provider, avoiding hardcoding the URL in multiple function blocks.

--- src/utils/ens.rs
+++ src/utils/ens.rs
@@ -3,10 +3,15 @@ use std::str::FromStr;
 use anyhow::{anyhow, Result};
 use ethers::{addressbook::Address, middleware::Middleware, prelude::Provider, providers::Http};
 
+const ETHEREUM_MAINNET_RPC_URL: &'static str = "https://eth.llamarpc.com";
+
+async fn create_provider() -> Result<Provider<Http>> {
+  Provider::<Http>::try_from(ETHEREUM_MAINNET_RPC_URL)
+    .map_err(|error| anyhow!("Failed to create provider from endpoint: {}", error))
+}
+
 pub async fn get_domain_name(address: &str) -> Result<String> {
-  let endpoint = "https://eth.llamarpc.com";
-  let provider = Provider::<Http>::try_from(endpoint)
-    .map_err(|error| anyhow!("Failed to create provider from endpoint: {}", error))?;
+  let provider = create_provider().await?;
 
   let ethereum_address =
     Address::from_str(address).map_err(|error| anyhow!("Failed to parse address: {}", error))?;
@@ -19,10 +24,8 @@ pub async fn get_domain_name(address: &str) -> Result<String> {
   Ok(domain_name)
 }
 
-pub async fn get_domain_field(domain: &str, field: &str) -> Result<String, anyhow::Error> {
-  let endpoint = "https://eth.llamarpc.com";
-  let provider = Provider::<Http>::try_from(endpoint)
-    .map_err(|error| anyhow!("Failed to create provider from endpoint: {}", error))?;
+pub async fn get_domain_field(domain: &str, field: &str) -> Result<String> {
+  let provider = create_provider().await?;
 
   let value = provider
     .resolve_field(domain, field)
