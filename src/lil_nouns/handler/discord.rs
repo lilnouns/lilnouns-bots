c8ba0f3983f4ad7746a16bf12f980088c836098c
Milad Nekofar <milad@nekofar.com>
2023-10-07 18:04:13 +0330

    refactor: add new functions to handle Discord messages for Lil Nouns
    The handler for the Discord channel is updated with the ability to handle new proposals and new votes. Functions `handle_new_proposal` and `handle_new_vote` are now implemented with logic to format the respective events into a Discord message embed and sending them via a webhook. This change is committed to enable alert notifications to a Discord channel when a new proposal is added or a new vote is made.

--- src/lil_nouns/handler/discord.rs
+++ src/lil_nouns/handler/discord.rs
@@ -1,10 +1,15 @@
 use async_trait::async_trait;
-use reqwest::Client;
-use worker::{Env, Result};
+use chrono::Local;
+use header::CONTENT_TYPE;
+use log::{error, info};
+use reqwest::{header, Client};
+use serde_json::{json, Value};
+use worker::{Env, Error, Result};
 
 use crate::{
   cache::Cache,
   lil_nouns::{handler::Handler, Proposal, Vote},
+  utils::{ens::get_domain_name, get_explorer_address, get_short_address},
 };
 
 pub(crate) struct DiscordHandler {
@@ -33,15 +38,106 @@ impl DiscordHandler {
 
     Ok(Self::new(base_url, webhook_url, cache, client))
   }
+
+  async fn execute_webhook(&self, embed: Value) -> Result<()> {
+    let msg_json = json!({"embeds": [embed]});
+
+    self
+      .client
+      .post(&self.webhook_url)
+      .header(CONTENT_TYPE, "application/json")
+      .body(msg_json.to_string())
+      .send()
+      .await
+      .map_err(|e| {
+        error!("Failed to execute webhook: {}", e);
+        Error::from(format!("Failed to execute webhook: {}", e))
+      })?;
+
+    Ok(())
+  }
 }
 
 #[async_trait(? Send)]
 impl Handler for DiscordHandler {
   async fn handle_new_proposal(&self, proposal: &Proposal) -> Result<()> {
-    todo!()
+    info!("Handling new proposal: {}", proposal.title);
+
+    let url = format!("{}/{}", self.base_url, proposal.id);
+    let date = Local::now().format("%m/%d/%Y %I:%M %p").to_string();
+    let wallet = get_domain_name(&proposal.proposer)
+      .await
+      .unwrap_or(get_short_address(&proposal.proposer));
+    let description = format!(
+      "A new Lil Nouns proposal has been created: “{}”",
+      proposal.title
+    );
+    let explorer = get_explorer_address(&proposal.proposer);
+
+    let embed = json!({
+        "title": "New Lil Nouns Proposal",
+        "description": description,
+        "url": url,
+        "color": 0x7BC4F2,
+        "footer": {"text": date},
+        "author": {
+            "name": wallet,
+            "url": explorer,
+        }
+    });
+
+    self.execute_webhook(embed).await?;
+
+    Ok(())
   }
 
   async fn handle_new_vote(&self, vote: &Vote) -> Result<()> {
-    todo!()
+    info!("Handling new vote from address: {}", vote.voter);
+
+    let proposals = self
+      .cache
+      .get::<Vec<Proposal>>("lil_nouns:proposals")
+      .await?
+      .unwrap();
+
+    let proposal = proposals
+      .iter()
+      .find(|&a| a.id == vote.proposal_id)
+      .unwrap()
+      .clone();
+
+    let url = format!("{}/{}", self.base_url, proposal.id);
+    let date = Local::now().format("%m/%d/%Y %I:%M %p").to_string();
+    let wallet = get_domain_name(&vote.voter)
+      .await
+      .unwrap_or(get_short_address(&vote.voter));
+
+    let description = format!(
+      "{} has voted “{}” proposal.",
+      wallet,
+      match vote.direction {
+        0 => "against",
+        1 => "for",
+        2 => "abstain on",
+        _ => "unknown",
+      }
+    );
+    let explorer = get_explorer_address(&vote.voter);
+
+    let embed = json!({
+        "title": "New Prop House Proposal Vote",
+        "description": description,
+        "url": url,
+        "color": 0x7BC4F2,
+        "footer": {"text": date},
+        "author": {
+            "name": wallet,
+            "url": explorer,
+        }
+    });
+
+    self.execute_webhook(embed).await?;
+
+    Ok(())
   }
 }
