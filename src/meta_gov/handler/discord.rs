7bfbd2b1ae0371def993eb6fc49d6567e320d417
Milad Nekofar <milad@nekofar.com>
2023-10-25 14:18:38 +0330

    refactor: handle null proposal scenario in discord handler
    Updated discord.rs to use unwrap_or_default() and ok_or() instead of unwrap() for retrieving proposals. This change makes the code safer by either returning a default value when a proposal isn't found, or an explicit error message instead of panicking."

--- src/meta_gov/handler/discord.rs
+++ src/meta_gov/handler/discord.rs
@@ -121,13 +121,13 @@ impl Handler for DiscordHandler {
       .cache
       .get::<Vec<Proposal>>("meta_gov:proposals")
       .await?
-      .unwrap();
+      .unwrap_or_default();
 
     let proposal = proposals
       .iter()
       .find(|&a| a.id == vote.proposal_id)
-      .unwrap()
-      .clone();
+      .clone()
+      .ok_or("Proposal not found in the funding list.")?;
 
     match self.extract_proposal_info(proposal.clone()).await {
       Ok((proposal_id, proposal_title)) => {
