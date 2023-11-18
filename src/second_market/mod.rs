e2cefc5486ac0da79e00568b2e47f86d2e5e65f6
Milad Nekofar <milad@nekofar.com>
2023-11-18 16:23:24 +0330

    refactor: add Floor structure to second market module
    A new structure named 'Floor' has been introduced in the second_market module. This structure, which is serializable, deserializable, cloneable, has a default, and can be debugged, contains several public fields: id, price, source, created_at, and previous_price.

--- src/second_market/mod.rs
+++ src/second_market/mod.rs
@@ -1,1 +1,12 @@
+use serde::{Deserialize, Serialize};
+
 pub(crate) mod fetcher;
+
+#[derive(Serialize, Deserialize, Clone, Default, Debug)]
+pub struct Floor {
+  pub id: String,
+  pub price: f64,
+  pub source: String,
+  pub created_at: String,
+  pub previous_price: f64,
+}
