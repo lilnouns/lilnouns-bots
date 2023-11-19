27db72ce63b0ce53e64ea477d428c0687eb3b670
Milad Nekofar <milad@nekofar.com>
2023-11-19 10:16:09 +0330

    refactor: implement farcaster handler to handle new second market floor
    The FarcasterHandler class has been updated to manage HTTP requests and caches. These changes enhance capabilities to interact with external APIs. The addition of the environment variable "SECOND_MARKET_WARP_CAST_CHANNEL" supports channel-specific communications.

--- src/second_market/handler/farcaster.rs
+++ src/second_market/handler/farcaster.rs
@@ -1,25 +1,118 @@
 use async_trait::async_trait;
-use log::debug;
-use worker::{Env, Result};
+use log::{debug, error, info};
+use reqwest::{
+  header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+  Client,
+  Response,
+};
+use serde_json::{json, Value};
+use worker::{Env, Error, Result};
 
-use crate::second_market::{handler::Handler, Floor};
+use crate::{
+  cache::Cache,
+  second_market::{handler::Handler, Floor},
+};
 
-pub(crate) struct FarcasterHandler {}
+pub(crate) struct FarcasterHandler {
+  warpcast_url: String,
+  bearer_token: String,
+  channel_key: String,
+  cache: Cache,
+  client: Client,
+}
 
 impl FarcasterHandler {
-  pub fn new() -> Self {
-    Self {}
+  pub fn new(
+    warpcast_url: String,
+    bearer_token: String,
+    channel_key: String,
+    cache: Cache,
+    client: Client,
+  ) -> Self {
+    Self {
+      warpcast_url,
+      bearer_token,
+      channel_key,
+      cache,
+      client,
+    }
   }
 
   pub fn new_from_env(env: &Env) -> Result<Self> {
-    Ok(Self::new())
+    let warpcast_url = env.var("WARP_CAST_API_BASE_URL")?.to_string();
+    let bearer_token = env.secret("SECOND_MARKET_WARP_CAST_TOKEN")?.to_string();
+    let channel_key = env.var("SECOND_MARKET_WARP_CAST_CHANNEL")?.to_string();
+
+    let cache = Cache::new_from_env(env);
+    let client = Client::new();
+
+    Ok(Self::new(
+      warpcast_url,
+      bearer_token,
+      channel_key,
+      cache,
+      client,
+    ))
+  }
+
+  async fn make_http_request(&self, request_data: Value) -> Result<Response> {
+    let url = format!("{}/casts", self.warpcast_url);
+    let token = format!("Bearer {}", self.bearer_token);
+    let mut headers = HeaderMap::new();
+
+    let parsed_token =
+      HeaderValue::from_str(&token).map_err(|_| Error::from("Error while parsing token"))?;
+
+    headers.insert(AUTHORIZATION, parsed_token);
+    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+
+    // Send the HTTP POST request
+    let response = self
+      .client
+      .post(url)
+      .headers(headers)
+      .json(&request_data)
+      .send()
+      .await
+      .map_err(|e| {
+        error!("Failed to execute request: {}", e);
+        Error::from(format!("Failed to execute request: {}", e))
+      })?;
+
+    debug!("Response status: {:?}", response.status());
+
+    Ok(response)
   }
 }
 
 #[async_trait(? Send)]
 impl Handler for FarcasterHandler {
   async fn handle_new_floor(&self, floor: &Floor) -> Result<()> {
-    debug!("{:#?}", floor);
+    info!("Handling new floor: {}", floor.id);
+
+    let url = match floor.clone().source.unwrap_or_else(String::new).as_str() {
+      "blur.io" => "https://blur.io/collection/lil-nouns",
+      _ => "https://opensea.io/collection/lil-nouns",
+    };
+
+    let description = format!(
+      "There has been a change in the floor price on the second market. The new floor price is \
+       new floor price is now {} Ξ, while the previous was {} Ξ.",
+      floor.new_price.unwrap().to_string(),
+      floor.old_price.unwrap().to_string()
+    );
+
+    let request_data = json!({
+      "text": description,
+      "embeds": [url],
+      "channelKey": self.channel_key
+    });
+
+    self.make_http_request(request_data).await.map_err(|e| {
+      error!("Failed to make HTTP request: {}", e);
+      return e;
+    })?;
 
     Ok(())
   }
