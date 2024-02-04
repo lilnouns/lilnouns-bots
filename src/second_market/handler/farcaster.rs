43aa6326d23fd0b416465060c07d8eb655cd3326
Milad Nekofar <milad@nekofar.com>
2024-02-04 10:34:50 +0330

    fix: solve embed url issue in second market farcaster handler

--- src/second_market/handler/farcaster.rs
+++ src/second_market/handler/farcaster.rs
@@ -103,11 +103,10 @@ impl Handler for FarcasterHandler {
       .unwrap_or_default();
     let new_price = collection.floor_ask.price.amount.decimal;
 
-    let mut url = format!(
+    let url = format!(
       "chain://eip155:1/erc721:{}/{}",
       collection.id, collection.floor_ask.token.token_id
     );
-    url = format!("{}?{}", url, now.timestamp());
 
     let description = format!(
       "There has been a change in the floor price on the second market. The new floor price is \
