c4212025842ac9f370620a430c806a7551546e43
Milad Nekofar <milad@nekofar.com>
2024-02-09 15:03:01 +0330

    revert: solve embed url issue in second market farcaster handler
    This reverts commit 43aa6326d23fd0b416465060c07d8eb655cd3326.

--- src/second_market/handler/farcaster.rs
+++ src/second_market/handler/farcaster.rs
@@ -101,10 +101,11 @@ impl Handler for FarcasterHandler {
       .unwrap_or_default();
     let new_price = collection.floor_ask.price.amount.decimal;
 
-    let url = format!(
+    let mut url = format!(
       "chain://eip155:1/erc721:{}/{}",
       collection.id, collection.floor_ask.token.token_id
     );
+    url = format!("{}?{}", url, now.timestamp());
 
     let description = format!(
       "There has been a change in the floor price on the second market. The new floor price is \
